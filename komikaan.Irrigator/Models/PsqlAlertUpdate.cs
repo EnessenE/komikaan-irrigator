3ddbff58b0a98e18cc36520775ca511652bfa203
Enes <9263360+EnessenE@users.noreply.github.com>
2024-12-14 12:52:06 +0100

    Expand alerts

--- komikaan.Irrigator/Models/PsqlAlertUpdate.cs
+++ komikaan.Irrigator/Models/PsqlAlertUpdate.cs
@@ -1,13 +1,22 @@
-﻿namespace komikaan.Irrigator.Models
+﻿using TransitRealtime;
+
+namespace komikaan.Irrigator.Models
 {
     internal class PsqlAlertUpdate
     {
-        public string data_origin { get; set; }
-        public Guid internal_id { get; set; }
-        public DateTimeOffset last_updated { get; set; }
-        public object agency_id { get; set; }
-        public object route_id { get; set; }
-        public object trip_id { get; set; }
-        public object stop_id { get; set; }
+        public string DataOrigin { get; set; }
+        public string Id { get; set; }
+        public Guid InternalId { get; set; }
+        public DateTimeOffset LastUpdated { get; set; }
+        public string? AgencyId { get; set; }
+        public string? RouteID { get; set; }
+        public string? TripId { get; set; }
+        public string? StopId { get; set; }
+        public Alert.Effect? Effect { get; internal set; }
+        public Alert.Cause? Cause { get; internal set; }
+        public Alert.SeverityLevel? SeverityLevel { get; internal set; }
+        public string? Url { get; internal set; }
+        public string? HeaderText { get; internal set; }
+        public string? DescriptionText { get; internal set; }
     }
 }
---
+++ komikaan.Irrigator/Services/PsqlActivePeriod.cs
@@ -0,0 +1,11 @@
+﻿
+namespace komikaan.Irrigator.Services
+{
+    internal class PsqlActivePeriod
+    {
+        public Guid alert_internal_id { get; set; }
+        public string data_origin { get; set; }
+        public DateTimeOffset? start_time { get; set; }
+        public DateTimeOffset? end_time { get; set; }
+    }
+}
