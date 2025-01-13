f30b1651373ef81ac10074869c9cf9515ebaea79
Enes <9263360+EnessenE@users.noreply.github.com>
2025-01-13 19:48:30 +0100

    0.3.12 - Headers and alerts

--- komikaan.Irrigator/Models/RealTimeFeed.cs
+++ komikaan.Irrigator/Models/RealTimeFeed.cs
@@ -7,5 +7,7 @@
         public bool Enabled { get; set; }
         public TimeSpan PollingRate { get; set; }
         public DateTimeOffset LastAttempt { get; set; }
+        public string? Header { get; set; }
+        public string? HeaderSecret { get; set; }
     }
 }
